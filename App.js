73502cfe69287011d896d7d937fbcba5bdd05126
Javier Soto <javislop@gmail.com>
2021-02-19 12:24:06 -0600

    add picker dependency

--- App.js
+++ App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
+import Formulario from './components/Formulario';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
+
+      <Image style={styles.imagen} 
+             source={require('./assets/img/cryptomonedas.png')} />
+      
+      <View style={styles.contenido}>
+        <Formulario />
+      </View>
+
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  imagen: {
+    width: '100%',
+    height: 150,
+  },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
 });
 
 export default App;
---
+++ components/Formulario.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Formulario = () => {
+  return (
+    <View>
+      <Text style={styles.label}>Moneda: </Text>
+
+      <Text style={styles.label}>Criptomoneda: </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  label: {
+    fontFamily: 'Lato-Black',
+    fontSize: 22,
+    marginVertical: 20,
+    textTransform: 'uppercase',
+  }
+})
+
+export default Formulario
+
